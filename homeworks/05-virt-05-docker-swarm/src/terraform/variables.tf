e216df770d2de766029cdd91f7d9ad1a88ae1e2b
Alexandr Belyaev <belas80@gmail.com>
2021-11-19 22:35:48 +0500

    [05-virt-05-docker-swarm] Done all tasks

--- homeworks/05-virt-05-docker-swarm/src/terraform/variables.tf
+++ homeworks/05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1gmh5fl71a23gh6qrqc"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1g9eq93ionckal26dpc"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd8jc7jjrdbl9gu47ubu"
 }
