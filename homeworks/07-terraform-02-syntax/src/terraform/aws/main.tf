49012800bbfd550719ef89ecb1d784895ab12624
Alexandr Belyaev <belas80@gmail.com>
2022-01-08 23:34:12 +0500

    [07-terraform-02-syntax] formated main.tf terraform AWS source

--- homeworks/07-terraform-02-syntax/src/terraform/aws/main.tf
+++ homeworks/07-terraform-02-syntax/src/terraform/aws/main.tf
@@ -19,10 +19,10 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name = "deployer-key"
+  key_name      = "deployer-key"
 
   tags = {
     Name = "HelloWorld"
