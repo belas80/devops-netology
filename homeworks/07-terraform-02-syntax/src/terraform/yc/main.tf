704fd1bf6fdea8e92f331ee4f4804188be2035ce
Alexandr Belyaev <belas80@gmail.com>
2022-01-09 15:26:13 +0500

    [07-terraform-02-syntax] added ya.cloud tf source

---
+++ homeworks/07-terraform-02-syntax/src/terraform/yc/main.tf
@@ -0,0 +1,40 @@
+provider "yandex" {
+  cloud_id  = "b1gmh5fl71a23gh6qrqc"
+  folder_id = "b1g9eq93ionckal26dpc"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8fte6bebi857ortlja"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
