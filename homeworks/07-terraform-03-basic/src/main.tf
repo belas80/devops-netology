776e8ea5b5ff8354963d1e611e2a4cceba2d4470
Alexandr Belyaev <belas80@gmail.com>
2022-01-17 23:41:23 +0500

    [07-terraform-03-basic] first commit

---
+++ homeworks/07-terraform-03-basic/src/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "deployer-key"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
